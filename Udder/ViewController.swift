09d4cd405235c8e44a319dababa37dbd6be4401c
Jade Jamig <ajamig016@gmail.com>
2020-09-03 07:05:00 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Anselm Jade Jamig on 9/3/20.
+//  Copyright © 2020 Anselm Jade Jamig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
